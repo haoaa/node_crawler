9a2f771031e9c96afafe63b91293e3669a66b706
haoaa <290058005@qq.com>
2019-03-11 19:39:06 +0800

    x

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
+  // required to lint *.vue files
+  // add your custom rules here
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    'semi': ['error', 'never'],
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    //custom config
+    'indent': ['error', 2],
+    'no-unused-vars': 0,
+    'space-before-function-paren': ['error', 'never'],
+    'comma-dangle': ['error', 'only-multiline'],
+    'key-spacing': ['error', { 'align': 'colon' }],
+    'eol-last': 0,
+    'object-curly-spacing': 0,
+  }
+}
